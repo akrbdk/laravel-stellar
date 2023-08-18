1659927fbd7f22126f839204ec8ca65cf6e60a36
alexander.karabudka <sanyakarabudka@gmail.com>
2023-08-18 23:14:44 +0300

    add service provider

---
+++ packages/icons/src/Providers/IconsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Akrbdk\Icons\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class IconsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
