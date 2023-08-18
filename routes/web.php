9512c780e29b590b45660b2dff430e12ec2c3ae2
alexander.karabudka <sanyakarabudka@gmail.com>
2023-08-18 17:56:02 +0300

    init base pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/generic', function () {
+    return view('generic');
 });
