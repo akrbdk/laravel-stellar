daf9e96009b8d9dfb18c8908944b200445e9247d
alexander.karabudka <sanyakarabudka@gmail.com>
2024-07-29 12:31:22 +0200

    modify layouts

---
+++ resources/views/vendor/platform/footer.blade.php
@@ -0,0 +1,17 @@
+@guest
+    <div class="m-4 text-center text-muted">
+
+    </div>
+
+    <ul class="nav justify-content-center mb-5">
+        <li class="nav-item"><a href="https://www.linkedin.com/in/akarabudka/" target="_blank" class="nav-link px-2 text-muted">LinkedIn</a></li>
+        <li class="nav-item"><a href="https://github.com/akrbdk" target="_blank" class="nav-link px-2 text-muted">GitHub</a></li>
+    </ul>
+@else
+
+    <div class="text-center user-select-none my-4 d-none d-lg-block">
+        <p class="small mb-0">
+
+        </p>
+    </div>
+@endguest
---
+++ resources/views/vendor/platform/header.blade.php
@@ -0,0 +1,17 @@
+@push('head')
+    <meta name="robots" content="noindex"/>
+    <meta name="google" content="notranslate">
+    <!-- For Safari on iOS -->
+    <meta name="theme-color" content="#21252a">
+@endpush
+
+<div class="h2 d-flex align-items-center">
+    @auth
+        <x-orchid-icon path="bs.house" class="d-inline d-xl-none"/>
+    @endauth
+
+
+    <p class="my-0 {{ auth()->check() ? 'd-none d-xl-block' : '' }}">
+        {{ config('app.name') }}
+    </p>
+</div>
